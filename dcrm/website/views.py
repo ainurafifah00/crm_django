d39325f0dc737ccbc8bc0423780ff9df3d5573e3
ainurAFIFAH <ainurafifah00@users.noreply.github.com>
2024-12-15 15:54:51 +0700

    admin login auth

--- dcrm/website/views.py
+++ dcrm/website/views.py
@@ -1,5 +1,19 @@
 from django.shortcuts import render
+from django.contrib.auth import authenticate, login, logout
+from django.contrib import messages
 
-# Create your views here.
+
+# 3 steps process:
+# view
+# function thing
+# url
+
 def home(request):
 	return render(request, 'home.html', {})
+
+def login_user(request):
+	pass
+
+def logout_user(request):
+	pass
+
